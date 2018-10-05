41163811ffaa93ec45fb2fe0a1cc4367b98e42e4
Benjamin Estes <benjamin.lee.estes@gmail.com>
2018-10-05 10:23:18 -0700

    Break up CLI code into something sensible.

--- crawl.go
+++ crawl.go
@@ -1,9 +1,15 @@
+// Copyright 2018 Benjamin Estes. All rights reserved.  Use of this
+// source code is governed by an MIT-style license that can be found
+// in the LICENSE file.
+
 package main
 
 import (
 	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -22,40 +28,62 @@ var config = &crawler.Config{
 }
 
 func main() {
-	if os.Args[1] == "schema" {
-		j, _ := json.MarshalIndent(schema.BQ, "", "\t")
-		fmt.Printf("%s\n", j)
-		return
-	}
+	schemaCommand := flag.NewFlagSet("schema", flag.ExitOnError)
+	spiderCommand := flag.NewFlagSet("spider", flag.ExitOnError)
+	listCommand := flag.NewFlagSet("list", flag.ExitOnError)
 
-	if len(os.Args) != 3 {
-		fmt.Fprintln(os.Stderr, "expected command and config file path")
+	if len(os.Args) < 2 {
+		log.Fatal(fmt.Errorf("expected command"))
 		os.Exit(1)
 	}
 
-	configJSON, err := ioutil.ReadFile(os.Args[2])
+	var c *crawler.Crawler
+
+	switch os.Args[1] {
+	case "schema":
+		schemaCommand.Parse(os.Args[2:])
+		doSchema()
+		return
+	case "spider":
+		spiderCommand.Parse(os.Args[2:])
+		if spiderCommand.NArg() < 1 {
+			log.Fatal(fmt.Errorf("expected location of config file"))
+		}
+		config := configFromFile(spiderCommand.Arg(0))
+		c = crawlSpider(config)
+	case "list":
+		listCommand.Parse(os.Args[2:])
+		if listCommand.NArg() < 1 {
+			log.Fatal(fmt.Errorf("expected location of config file"))
+		}
+		config := configFromFile(listCommand.Arg(0))
+		queue := listFromReader(os.Stdin)
+		c = crawlList(config, queue)
+	default:
+		flag.PrintDefaults()
+		log.Fatal(fmt.Errorf("unknown command"))
+	}
+
+	doCrawl(c)
+}
+
+func configFromFile(name string) *crawler.Config {
+	config := &crawler.Config{}
+
+	configJSON, err := ioutil.ReadFile(name)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	err = json.Unmarshal(configJSON, config)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "couldn't parse json from config file")
-		os.Exit(0)
+		log.Fatal(err)
 	}
 
-	var c *crawler.Crawler
+	return config
+}
 
-	switch os.Args[1] {
-	case "site":
-		c = crawlSite(config)
-	case "list":
-		c = crawlList(config)
-	default:
-		fmt.Fprintln(os.Stderr, "Invalid command.")
-		os.Exit(1)
-	}
-
+func doCrawl(c *crawler.Crawler) {
 	count := 0
 	start := time.Now()
 	for n := c.Next(); n != nil; n = c.Next() {
@@ -74,17 +102,25 @@ func main() {
 	fmt.Fprintf(os.Stderr, "\n")
 }
 
-func crawlSite(config *crawler.Config) *crawler.Crawler {
-	return crawler.Crawl(config)
+func doSchema() {
+	j, _ := json.MarshalIndent(schema.BQ, "", "\t")
+	fmt.Printf("%s\n", j)
 }
 
-func crawlList(config *crawler.Config) *crawler.Crawler {
-	var queue []*data.Address
-	scanner := bufio.NewScanner(os.Stdin)
+func listFromReader(in io.Reader) (queue []*data.Address) {
+	scanner := bufio.NewScanner(in)
 	for scanner.Scan() {
 		n := data.MakeAddressFromString(scanner.Text())
 		queue = append(queue, n)
 	}
+	return
+}
+
+func crawlSpider(config *crawler.Config) *crawler.Crawler {
+	return crawler.Crawl(config)
+}
+
+func crawlList(config *crawler.Config, queue []*data.Address) *crawler.Crawler {
 	config.MaxDepth = 0
 	return crawler.CrawlList(config, queue)
 }
