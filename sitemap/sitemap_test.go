1e1dd44b760fe7be13448733c727de68cf6825e5
Benjamin Estes <benjamin.lee.estes@gmail.com>
2018-10-17 09:57:34 -0700

    Add httptest and testdata for package sitemap.

--- sitemap/sitemap_test.go
+++ sitemap/sitemap_test.go
@@ -2,14 +2,55 @@ package sitemap
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 )
 
-func TestFromURL(t *testing.T) {
-	urls, err := Fetch("https://www.distilled.net/sitemap.xml")
+func TestFetch(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open("testdata/sitemap.xml")
+		if err != nil {
+			t.Errorf("couldn't open sitemap.xml for reading")
+		}
+		data, err := ioutil.ReadAll(f)
+		if err != nil {
+			t.Errorf("couldn't read sitemap.xml")
+		}
+		fmt.Fprintf(w, "%s", data)
+	}))
+
+	urls, err := Fetch(ts.URL)
 	if err != nil {
-		fmt.Println(err)
+		t.Errorf("couldn't retrieve test sitemap")
 	}
 
-	fmt.Println(len(urls))
+	if len(urls) != 1 {
+		t.Errorf("expected 1 URL, got %d", len(urls))
+	}
+}
+
+func TestFetchIndex(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open("testdata/sitemap-index.xml")
+		if err != nil {
+			t.Errorf("couldn't open sitemap-index.xml for reading")
+		}
+		data, err := ioutil.ReadAll(f)
+		if err != nil {
+			t.Errorf("couldn't read sitemap-index.xml")
+		}
+		fmt.Fprintf(w, "%s", data)
+	}))
+
+	urls, err := FetchIndex(ts.URL)
+	if err != nil {
+		t.Errorf("couldn't retrieve test sitemap index")
+	}
+
+	if len(urls) != 2 {
+		t.Errorf("expected 2 URLs, got %d", len(urls))
+	}
 }
